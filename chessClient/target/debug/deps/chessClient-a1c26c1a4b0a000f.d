3c9d725673162c9accb83393625b6fc139878897
Erik Vander Laan <erbear299@gmail.com>
2022-07-17 08:34:13 -0500

    Initialize repository

---
+++ chessClient/target/debug/deps/chessClient-a1c26c1a4b0a000f.d
@@ -0,0 +1,5 @@
+c:\Users\Erik\Programming Projects\Rust Development\RustChessGame\RustChessGame\chessClient\target\debug\deps\chessClient-a1c26c1a4b0a000f.rmeta: src\main.rs
+
+c:\Users\Erik\Programming Projects\Rust Development\RustChessGame\RustChessGame\chessClient\target\debug\deps\chessClient-a1c26c1a4b0a000f.d: src\main.rs
+
+src\main.rs:
---
+++ chessClient/target/debug/deps/chessClient-ab1caca153ad7ad0.d
@@ -0,0 +1,5 @@
+c:\Users\Erik\Programming Projects\Rust Development\RustChessGame\RustChessGame\chessClient\target\debug\deps\chessClient-ab1caca153ad7ad0.rmeta: src\main.rs
+
+c:\Users\Erik\Programming Projects\Rust Development\RustChessGame\RustChessGame\chessClient\target\debug\deps\chessClient-ab1caca153ad7ad0.d: src\main.rs
+
+src\main.rs:
